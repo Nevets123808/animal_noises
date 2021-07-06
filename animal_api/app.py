49c44e3207ccbe0213afd220785421cfd5d25859
Steven Bourn <stevenbourn2@hotmail.com>
2021-07-06 15:55:04 +0000

    basic functionality

--- animal_api/app.py
+++ animal_api/app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from random import choice
+
 
 app = Flask(__name__)
-
+animals = {"pig":"oink", "cow":"moo", "sheep":"baa"}
 # animal generator route here
+@app.route("/getanimal")
+def get_animal():
+    animal = choice(list(animals.keys()))
+    print(animal)
+    return jsonify({"data": animal})
+
+@app.route("/getnoise", methods = ["POST"])
+def get_noise():
+    package = request.get_json()
+    animal = package["data"]
+    noise = animals[animal]
+    return jsonify({"data": noise})
+    
 
 # animal noise generator route here
 
--- server/app.py
+++ server/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
+api = "http://api:5000"
 
 # home route here
 # must query the animal API for an animal and a noise – the noise should be based on the animal
+@app.route("/")
+def home():
+    animal = requests.get(api + "/getanimal")
+    
+    noise = requests.post(api + "/getnoise", json = animal.json())
+    return f"The {animal.json()['data']} goes {noise.json()['data']}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
